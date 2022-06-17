4514becf8584acbeb02aff4091242141f01f6f54
Felix <felix-schindler@outlook.com>
2022-06-17 23:23:48 +0200

    Upd about txt; Update blog

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
-import blog from "https://felix-schindler.github.io/deno_blog/blog.tsx";
+import blog from "https://deno.land/x/blog_fork@1.0.0/blog.tsx";
 
 blog({
   title: "My Blog",
-  about: "Hey, I am an 20-year-old German student who loves to code. My future degree will be Bachelor of Engineering as a German-Chinese double-degree. I am currently studying Digital Media Technology, which is computer science with the addition of the Chinese language.",
+  about: "Hey I'm a 20 year old German student who loves to code. I am currently studying Computer Science and Media as a German-Chinese double degree.",
   description: "thoughts and prayers",
   avatar: "https://avatars.githubusercontent.com/u/65357346",
   avatarClass: "rounded-full",
   author: "Felix",
   links: [
     { title: "eMail", url: "mailto:webmaster@schindlerfelix.de" },
-    { title: "GitHub", url: "https://github.com/felix-schindler" },
-    { title: "Website", url: "https://schindlerfelix.de" },
+    { title: "GitHub", url: "https://github.com/felix-schindler" }
   ],
   background: "#F9F9F9",
 });
