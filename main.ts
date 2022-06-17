63e8cc68dfd9f7eed3642cff4614c5156993c021
Felix <felix-schindler@outlook.com>
2022-06-17 15:58:32 +0200

    Add about; use my fork of blog

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+import blog from "https://felix-schindler.github.io/deno_blog/blog.tsx";
 
 blog({
   title: "My Blog",
+  about: "Hey, I am an 20-year-old German student who loves to code. My future degree will be Bachelor of Engineering as a German-Chinese double-degree. I am currently studying Digital Media Technology, which is computer science with the addition of the Chinese language.",
   description: "thoughts and prayers",
   avatar: "https://avatars.githubusercontent.com/u/65357346",
   avatarClass: "rounded-full",
