bda5bbffc4ee341b65d59056fd1b263b169e75e4
Felix <felix-schindler@outlook.com>
2023-03-18 11:02:55 +0100

    Add gitlab and it's privacy page

---
+++ src/routes/projects/gitlab/+page.svelte
@@ -0,0 +1,40 @@
+<h1>
+	<img
+		src="https://gitlab.com/uploads/-/system/project/avatar/33025310/Tanuki-200kb.png"
+		alt="Tanuki app logo"
+	/>
+	Tanuki for GitLab
+</h1>
+
+<p class="subtitle">
+	The native GitLab client for iOS and iPadOS, made with SwiftUI.
+</p>
+
+<div class="grid">
+	<img src="/img/tanuki/home.png" alt="" />
+	<img src="/img/tanuki/project.png" alt="" />
+	<img src="/img/tanuki/issues.png" alt="" />
+	<img src="/img/tanuki/branches.png" alt="" />
+</div>
+
+<style>
+	h1 {
+		display: flex;
+		align-items: center;
+		gap: 0.25em;
+		flex-wrap: wrap;
+	}
+
+	h1 img {
+		max-height: 1.25em;
+	}
+
+	p.subtitle {
+		font-size: larger;
+	}
+
+	img {
+		border-radius: var(--radius);
+		max-width: 100%;
+	}
+</style>
