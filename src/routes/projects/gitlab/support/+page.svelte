61b7dc8a1083f29a9336593433fab7a1a3169f7c
Felix <felix-schindler@outlook.com>
2023-03-18 11:43:17 +0100

    Create gitlab support page
    Just opens a new mail with the gitlab helpdesk email as recipient

---
+++ src/routes/projects/gitlab/support/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	let title: string, msg: string;
+
+	async function sendSupport() {
+		open(
+			"mailto:contact-project+felix-schindler-gitlab-ios-33025310-issue-@incoming.gitlab.com" +
+				"?subject=" +
+				encodeURIComponent(title) +
+				"&body=" +
+				encodeURIComponent(msg)
+		);
+	}
+</script>
+
+<div class="ac">
+	<form class="card" on:submit|preventDefault={sendSupport}>
+		<input placeholder="Title" type="text" bind:value={title} />
+		<textarea placeholder="Message" cols="30" rows="10" bind:value={msg} />
+		<input type="submit" disabled={!title || !msg} />
+	</form>
+</div>
+
+<style>
+	input[type="submit"]:not(:disabled) {
+		cursor: pointer;
+	}
+</style>
