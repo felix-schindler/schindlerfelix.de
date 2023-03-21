42c58d120349a7603b3661ae647e4dcd52d3d5d9
Felix <felix-schindler@outlook.com>
2023-03-21 21:52:01 +0100

    Add gitlab support faq section

--- src/routes/projects/gitlab/support/+page.svelte
+++ src/routes/projects/gitlab/support/+page.svelte
@@ -14,15 +14,63 @@
 
 <div class="ac">
 	<form class="card" on:submit|preventDefault={sendSupport}>
-		<h2>Tanuki Support</h2>
+		<h1>Tanuki Support</h1>
 		<input placeholder="Title" type="text" bind:value={title} />
 		<textarea placeholder="Message" cols="30" rows="10" bind:value={msg} />
-		<input type="submit" disabled={!title || !msg} />
+		<button type="submit" disabled={!title || !msg}>Send</button>
 	</form>
 </div>
 
+<div class="faq">
+	<h1 id="faq"><a href="#faq">FAQ</a></h1>
+	<div class="grid">
+		<div class="card">
+			<h3>Why does the app cost anything?</h3>
+			<p>
+				Apple charges money for developer accounts, which you need to be able to
+				deploy apps to the App Store. <small
+					>Also, I shouldn't work for free forever.</small
+				>
+			</p>
+			<p>
+				I'm not a fan of monthly subscriptions, so that's why there's just the
+				one-time-cost.
+			</p>
+		</div>
+		<div class="card">
+			<h3>How to submit a feature request?</h3>
+			<p>
+				Just fill in the form above and send the email, or head over to <a
+					href="https://gitlab.com/felix-schindler/gitlab-ios">the repo</a
+				>
+				and open an issue yourself.
+			</p>
+			<p>You can also just open a merge request if you'd like!</p>
+		</div>
+		<div class="card">
+			<h3>When will the app be feature complete?</h3>
+			<p>
+				Feature completeness is a hard thing to achive as GitLab is a pretty big
+				software.
+			</p>
+			<p>
+				The goal for the beginning is to support the most used features by most
+				users. After that, the focus is on whatever the users suggest. When
+				there are no suggestions the app will receivice incremental updates with
+				no specific focus.
+			</p>
+		</div>
+	</div>
+</div>
+
 <style>
-	input[type="submit"]:not(:disabled) {
-		cursor: pointer;
+	@media (max-width: 768px) {
+		.ac {
+			height: auto;
+		}
+	}
+
+	div.faq {
+		padding-bottom: 1em;
 	}
 </style>
