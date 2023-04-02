4e7a87e96b96200176e064444b814c8d06ec1d5b
Felix <felix-schindler@outlook.com>
2023-04-02 15:16:37 +0200

    x

---
+++ src/routes/(reset)/photos/china/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	/** Whether is viewed on a small screen */
+	let isPrettyAverage: boolean;
+	let parallaxTitle: HTMLElement;
+
+	function onScroll() {
+		const scrollTop = window.scrollY;
+
+		if (isPrettyAverage) {
+			parallaxTitle.style.top = scrollTop * -0.5 + "px";
+		} else {
+			parallaxTitle.style.top = scrollTop * -0.3 + "px";
+		}
+	}
+
+	onMount(() => {
+		isPrettyAverage = window.innerWidth < 768;
+	});
+</script>
+
+<svelte:window on:scroll={onScroll} />
+
+<div class="hero">
+	<div class="hero-img hero-img-mask" />
+	<div class="parallax-title" bind:this={parallaxTitle}>
+		<h1 class="hero-title">China</h1>
+	</div>
+	<div class="hero-img hero-img-bg" />
+</div>
+<div style="height: 100vh;">
+	<h2>Sources</h2>
+	<a href="https://eliasruiz.com/china">Elias Ruiz</a>
+</div>
+
+<style>
+	/* Overwrite */
+	.hero-img-mask {
+		background-image: url("/img/china/mask.png");
+	}
+
+	.hero-img-bg {
+		background-image: url("/img/china/china.jpg");
+	}
+
+	.hero-title {
+		font-size: 20vw;
+		bottom: 10%;
+	}
+
+	@media screen and (max-width: 768px) {
+		.hero-title {
+			bottom: 63%;
+			font-size: 30vw;
+		}
+	}
+</style>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,4 +2,20 @@
 	import { page } from "$app/stores";
 </script>
 
-<h1>{$page.status} <small>- {$page.error?.message}</small></h1>
+<div class="ac">
+	<h1>{$page.status} <small>- {$page.error?.message}</small></h1>
+	<div class="row">
+		<a href="/" role="button">Home</a>
+		<span>|</span>
+		<button class="inverse" on:click={() => history.back()}>Back</button>
+	</div>
+</div>
+
+<style>
+	.row {
+		display: flex;
+		align-items: center;
+		flex-wrap: wrap;
+		gap: 1em;
+	}
+</style>
