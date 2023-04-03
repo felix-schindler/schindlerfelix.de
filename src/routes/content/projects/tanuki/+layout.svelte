3bc056687d722c680117370b843aad6c091840e5
Felix <felix-schindler@outlook.com>
2023-04-03 18:18:41 +0200

    Add tanuki header for all tanuki pages; Fix heading order

---
+++ src/routes/content/projects/tanuki/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { page } from "$app/stores";
+
+	let path;
+	$: path = $page.url.pathname;
+</script>
+
+<h1>
+	<img
+		src="https://gitlab.com/uploads/-/system/project/avatar/33025310/Tanuki-200kb.png"
+		alt="Tanuki app icon"
+	/>
+	Tanuki for GitLab
+</h1>
+
+<p class="subtitle">
+	Native GitLab client for iOS and iPadOS, made with SwiftUI.
+</p>
+
+{#if path != "/content/projects/tanuki"}
+	<a href="/content/projects/tanuki">&larr; Back</a>
+{/if}
+
+<slot />
+
+<style>
+	h1 img {
+		border-radius: var(--radius);
+		vertical-align: bottom;
+		max-height: 1.25em;
+	}
+
+	p.subtitle {
+		font-size: larger;
+	}
+</style>
