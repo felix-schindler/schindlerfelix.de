23fc8f30dc1900cc537e0f9578715fb0626401ea
Felix <felix-schindler@outlook.com>
2023-04-03 18:03:54 +0200

    Everything.
    - Create Markdown component
    - Make .grid children at least 250px wide
    - Move markdown and prism css files to lib
    - Add global label and svg.bi style
    - Move all the pages to the top folder again
    - Create new `/content` route with blog, projects and photos as children
    - Keep `/projects/tanuki` as backup

---
+++ src/routes/content/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from "$app/stores";
+	import Markdown from "$lib/components/Markdown.svelte";
+
+	let path;
+	$: path = $page.url.pathname;
+</script>
+
+{#if !path.includes("/photos/")}
+	<Markdown>
+		<slot />
+	</Markdown>
+{:else}
+	<slot />
+{/if}
