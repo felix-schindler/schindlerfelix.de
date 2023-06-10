f399faa18ecb5405548ac97db6ff2d5622f9b8a6
Felix <felix-schindler@outlook.com>
2023-06-10 16:58:34 +0900

    perf(layout.ts): Import browser inside function

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,26 +1,30 @@
-import { browser } from '$app/environment';
 import { loadTranslations, locale } from '$lib/translations';
 
 export async function load({ url }) {
 	const { pathname } = url;
-	const initLocale = locale.get() || getDefaultLocale(); // set default if no locale already set
+	const initLocale = locale.get() || await getDefaultLocale(); // set default if no locale already set
 	await loadTranslations(initLocale, pathname); // keep this just before the `return`
 
 	return {};
 }
 
-function getDefaultLocale(): string {
+async function getDefaultLocale(): Promise<string> {
 	enum LANG {
 		DE = 'de',
 		EN = 'en',
 		ZH = 'zh',
 	};
 
-	let defaultLocale: string = LANG.EN; // get from cookie, user session, ...
+	let defaultLocale: string = LANG.EN;
 
+	// Get from navigator (browser)
+	const { browser } = await import('$app/environment');
 	if (browser) {
 		const navLang = navigator.language;
+
+		// Check whether the navigator language if supported
 		if (navLang.startsWith(LANG.EN)) {
+			// Already set to EN
 		} else if (navLang.startsWith(LANG.DE)) {
 			defaultLocale = LANG.DE;
 		} else if (navLang.startsWith(LANG.ZH)) {
