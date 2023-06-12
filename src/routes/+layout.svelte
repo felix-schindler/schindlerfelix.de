445f1259cd3965eb1e9333638ff786111977c840
Felix <felix-schindler@outlook.com>
2023-06-12 22:00:26 +0200

    Add view count via blog-api to footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,22 @@
-<script>
+<script lang="ts">
 	import "$lib/style/main.css"; // Global styling
 	import { page } from "$app/stores";
 	import { locale, t } from "$lib/translations";
+	import { onMount } from "svelte";
 
-	let path;
+	let path: string,
+		style: string,
+		views = 0;
 	$: path = $page.url.pathname;
-
-	let style;
 	$: path.includes("/photos/")
 		? (style = "padding: 0")
 		: (style = "padding: 0 1em");
+
+	// TODO: Move this to the `load` function
+	onMount(async () => {
+		const res = await fetch("https://blog-api.deno.dev/views");
+		views = Number(await res.text());
+	});
 </script>
 
 <div class="hmf" lang={$locale}>
@@ -28,16 +35,23 @@
 		<slot />
 	</main>
 	<footer>
-		<span
-			>{$t("common.made_by.pre_name")}<a href="/about"
+		<div
+			title={`${$t("common.since")} ${new Date(
+				"2023-06-12"
+			).toLocaleDateString()}`}
+		>
+			{$t("common.views")}<span class="mono">{views}</span>
+		</div>
+		<div>
+			{$t("common.made_by.pre_name")}<a href="/about"
 				>{$t("common.name.first")}</a
 			>{$t("common.made_by.pre_location")}
 			<a href="https://theländ.de" class="laend" rel="noopener noreferrer"
 				>The Länd</a
 			>
-			{$t("common.made_by.after_location")}</span
-		>
-		<span class="links">
+			{$t("common.made_by.after_location")}
+		</div>
+		<div class="links">
 			<a href="/imprint">{$t("common.imprint")}</a>
 			<a href="https://github.com/felix-schindler/blog">{$t("common.typo")}</a>
 			<select bind:value={$locale}>
@@ -46,7 +60,7 @@
 				<!-- <option value="kr">한국어</option> -->
 				<option value="zh-Hans">简体中文</option>
 			</select>
-		</span>
+		</div>
 	</footer>
 </div>
 
@@ -59,7 +73,7 @@
 		footer {
 			display: flex;
 			align-items: center;
-			flex-direction: column-reverse;
+			flex-direction: column;
 			gap: 0.75em;
 
 			@media (max-width: 1024px) {
