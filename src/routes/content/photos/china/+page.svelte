4436ac431d264fdd50ce0fa6b255dc0742708413
Felix <felix-schindler@outlook.com>
2024-01-28 00:06:35 +0800

    Move mask and sky images to new path

--- src/routes/content/photos/china/+page.svelte
+++ src/routes/content/photos/china/+page.svelte
@@ -1,39 +1,39 @@
 <script lang="ts">
-  import ImageCollection from "$lib/components/ImageCollection.svelte";
-  import Markdown from "$lib/components/Markdown.svelte";
-  import type { PageData } from "./$types";
+	import ImageCollection from "$lib/components/ImageCollection.svelte";
+	import Markdown from "$lib/components/Markdown.svelte";
+	import type { PageData } from "./$types";
 
-  export let data: PageData;
-  const baseImagePath = "/img/photos/china";
+	export let data: PageData;
+	const baseImagePath = "/img/photos/china";
 </script>
 
 <ImageCollection
-  country={{
-    name: "China",
-    mask: `${baseImagePath}/mask.png`,
-    sky: `${baseImagePath}/sky.jpg`,
-  }}
-  cities={[
-    {
-      images: data.files.map((f) => `${baseImagePath}/${f}`),
-    },
-  ]}
+	country={{
+		name: "China",
+		mask: "/img/photos/mask.png",
+		sky: "/img/photos/sky.jpg",
+	}}
+	cities={[
+		{
+			images: data.files.map((f) => `${baseImagePath}/${f}`),
+		},
+	]}
 >
-  <Markdown>
-    <h2>Sources</h2>
-    <p>
-      This is just a placeholder for now, until I start adding pictures.
-      Inspiration and pictures by <a href="https://eliasruiz.com/china"
-        >Elias Ruiz</a
-      >
-    </p>
-  </Markdown>
+	<Markdown>
+		<h2>Sources</h2>
+		<p>
+			This is just a placeholder for now, until I start adding pictures.
+			Inspiration and pictures by <a href="https://eliasruiz.com/china"
+				>Elias Ruiz</a
+			>
+		</p>
+	</Markdown>
 </ImageCollection>
 
 <style>
-  /* Overwrite */
-  :global(.hero-title) {
-    font-size: 20vw;
-    bottom: 10%;
-  }
+	/* Overwrite */
+	:global(.hero-title) {
+		font-size: 20vw;
+		bottom: 10%;
+	}
 </style>
