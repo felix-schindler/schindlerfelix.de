6b9fca8e76b23ab1e5af38773b9503a9343c3fd5
Felix <felix-schindler@outlook.com>
2024-01-30 02:54:49 +0800

    Fix image file extensions (→ .avif)

--- routes/photos/germany.tsx
+++ routes/photos/germany.tsx
@@ -12,22 +12,22 @@ export default function Germany() {
 				{
 					name: "Stuttgart",
 					images: [
-						"/img/photos/germany/stgt/1.jpeg",
-						"/img/photos/germany/stgt/2.jpeg",
-						"/img/photos/germany/stgt/3.jpeg",
-						"/img/photos/germany/stgt/4.jpeg",
-						"/img/photos/germany/stgt/5.jpeg",
+						"/img/photos/germany/stgt/1.avif",
+						"/img/photos/germany/stgt/2.avif",
+						"/img/photos/germany/stgt/3.avif",
+						"/img/photos/germany/stgt/4.avif",
+						"/img/photos/germany/stgt/5.avif",
 					],
 				},
 				{
 					name: "Berlin",
 					images: [
-						"/img/photos/germany/berlin/1.jpeg",
-						"/img/photos/germany/berlin/2.jpeg",
-						"/img/photos/germany/berlin/3.jpeg",
-						"/img/photos/germany/berlin/4.jpeg",
-						"/img/photos/germany/berlin/5.jpeg",
-						"/img/photos/germany/berlin/6.jpeg",
+						"/img/photos/germany/berlin/1.avif",
+						"/img/photos/germany/berlin/2.avif",
+						"/img/photos/germany/berlin/3.avif",
+						"/img/photos/germany/berlin/4.avif",
+						"/img/photos/germany/berlin/5.avif",
+						"/img/photos/germany/berlin/6.avif",
 					],
 				},
 			]}
