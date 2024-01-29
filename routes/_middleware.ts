84a8e0d6ee8c5b01b0ff221867af424badf6cad6
Felix <felix-schindler@outlook.com>
2024-01-30 02:47:06 +0800

    Fix redirect middleware

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -4,10 +4,12 @@ export async function handler(
 	req: Request,
 	ctx: FreshContext,
 ): Promise<Response> {
-	const { pathname } = new URL(req.url);
+	const currentUrl = new URL(req.url);
+	const { pathname } = currentUrl;
 
 	if (pathname.startsWith("/content")) {
-		return Response.redirect(pathname.replace("/content", ""), 302);
+		currentUrl.pathname = pathname.replace("/content", "");
+		return Response.redirect(currentUrl, 302);
 	}
 
 	return await ctx.next();
