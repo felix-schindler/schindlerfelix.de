bd5142feab5b3546642e62286eb3abf116b58f6d
Felix <felix-schindler@outlook.com>
2024-03-15 09:06:49 +0800

    Add tanuki v2 section

--- routes/projects/tanuki/index.tsx
+++ routes/projects/tanuki/index.tsx
@@ -2,6 +2,7 @@ import translations from "@/core/i18n/projects/tanuki/home.json" with {
 	type: "json",
 };
 
+import { Tech, Title } from "@/components/mod.tsx";
 import IconBrandGitlab from "icons/brand-gitlab.tsx";
 import IconMessageQuestion from "icons/message-question.tsx";
 import IconFileText from "icons/file-text.tsx";
@@ -29,23 +30,78 @@ export default function Tanuki(props: PageProps<never, State>) {
 	return (
 		<div class="flex flex-col gap-5">
 			<div>
-				<h2 class="text-3xl mb-2 font-bold tracking-tight">
+				<Title>
 					{translations[lang].tldr.heading}
-				</h2>
-				<p class="mb-1">
-					{translations[lang].tldr.pricing.replace("{:price}", price)}
-				</p>
-				<p class="mb-1">
-					{translations[lang].tldr.disclaimer}
-				</p>
-				<p>
-					{translations[lang].tldr.open}
-				</p>
+				</Title>
+				<div class="flex flex-col gap-1">
+					<p>
+						{translations[lang].tldr.pricing.replace("{:price}", price)}
+					</p>
+					<p>
+						{translations[lang].tldr.disclaimer}
+					</p>
+					<p>
+						{translations[lang].tldr.open}
+					</p>
+				</div>
 			</div>
 			<div>
-				<h2 class="text-3xl mb-2 font-bold tracking-tight">
+				<Title>
+					{translations[lang].v2.heading}
+				</Title>
+				<div class="flex flex-col gap-1">
+					<p>{translations[lang].v2.release_date}</p>
+					<p>{translations[lang].v2.txt}</p>
+					<details>
+						<summary class="cursor-pointer">
+							{translations[lang].v2.pics}
+						</summary>
+
+						<div class="grid grid-cols-default gap-2">
+							<img
+								src="/img/tanuki/v2/home.avif"
+								alt="Home"
+								loading="lazy"
+								class="rounded-lg"
+							/>
+							<img
+								src="/img/tanuki/v2/project.avif"
+								alt="Home"
+								loading="lazy"
+								class="rounded-lg"
+							/>
+							<img
+								src="/img/tanuki/v2/issues.avif"
+								alt="Home"
+								loading="lazy"
+								class="rounded-lg"
+							/>
+							<img
+								src="/img/tanuki/v2/issue.avif"
+								alt="Home"
+								loading="lazy"
+								class="rounded-lg"
+							/>
+							<img
+								src="/img/tanuki/v2/mr.avif"
+								alt="Home"
+								loading="lazy"
+								class="rounded-lg"
+							/>
+							<img
+								src="/img/tanuki/v2/group-expanded.avif"
+								alt="Home"
+								loading="lazy"
+								class="rounded-lg"
+							/>
+						</div>
+					</details>
+				</div>
+			</div>
+			<div>
+				<Title>
 					{translations[lang].links.heading}
-				</h2>
+				</Title>
 				<ul class="flex flex-wrap gap-2 items-center">
 					<li>
 						<a
@@ -58,7 +114,7 @@ export default function Tanuki(props: PageProps<never, State>) {
 					</li>
 					<li>
 						<a
-							href="https://www.schindlerflorian.de/tanuki/"
+							href="https://www.schindlerflorian.de/projects/tanuki/"
 							class="flex items-center gap-1 py-2 px-4 rounded-md border border-green-500 transition-all hover:scale-95"
 						>
 							<IconBrandAndroid color="rgb(34, 197, 94)" />
@@ -86,34 +142,42 @@ export default function Tanuki(props: PageProps<never, State>) {
 				</ul>
 			</div>
 			<div>
-				<h2 class="text-3xl mb-2 font-bold tracking-tight">
+				<Title>{translations[lang].tech}</Title>
+				<div class="flex flex-wrap gap-0.5">
+					<Tech name="Swift" />
+					<Tech name="SwiftUI" icon={false} />
+					<Tech name="GraphQL" icon={false} />
+				</div>
+			</div>
+			<div>
+				<Title>
 					{translations[lang].screenshots}
-				</h2>
+				</Title>
 				<div class="grid grid-cols-default gap-2">
 					<img
 						src="/img/tanuki/home.avif"
 						alt="Home"
 						loading="lazy"
 						class="rounded-lg"
-					/>{" "}
+					/>
 					<img
 						src="/img/tanuki/project.avif"
 						alt="Project"
 						loading="lazy"
 						class="rounded-lg"
-					/>{" "}
+					/>
 					<img
 						src="/img/tanuki/issues.avif"
 						alt="Issues"
 						loading="lazy"
 						class="rounded-lg"
-					/>{" "}
+					/>
 					<img
 						src="/img/tanuki/branches.avif"
 						alt="Branches"
 						loading="lazy"
 						class="rounded-lg"
-					/>{" "}
+					/>
 					<img
 						src="/img/tanuki/group.avif"
 						alt="Group"
