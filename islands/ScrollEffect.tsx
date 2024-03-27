a8138bb0d302ae1e02c11dadeabd8787c9d00dff
Felix <felix-schindler@outlook.com>
2024-03-27 21:58:07 +0800

    ScrollEffect: Add link to StackOverflow answer

--- islands/ScrollEffect.tsx
+++ islands/ScrollEffect.tsx
@@ -4,6 +4,7 @@ export default function ScollEffect() {
 	useEffect(() => {
 		globalThis.addEventListener("scroll", () => {
 			// @ts-expect-error - This is defined by the magic of old-school JS
+			// https://stackoverflow.com/a/3434388
 			globalThis.parallaxTitle.style.top = globalThis.scrollY * -0.9 + "px";
 		});
 	}, []);
