9ce1fd04a725101280e61c4ea8414843954a2739
Felix <felix-schindler@outlook.com>
2024-09-25 01:28:31 +0200

    localized notes from pb

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,9 +8,28 @@ import {
 } from "@/components/home/mod.tsx";
 import type { PageProps } from "fresh";
 import { State } from "@/utils.ts";
+import { pb } from "@/core/mod.ts";
 
-export default function Home(props: PageProps<never, State>) {
+const notesHeading = {
+	"en": "Notes",
+	"de": "Notizen",
+	"zh": "笔记",
+} as const;
+
+export default async function Home(props: PageProps<never, State>) {
 	const language = props.state.language;
+
+	const notes = (await pb.collection("notes").getFullList({
+		expand: language,
+		fields: "slug,expand",
+	}))
+		.map((note) => {
+			console.log(note);
+			return {
+				slug: note.slug,
+				...note.expand[language]!,
+			};
+		});
 
 	return (
 		<>
@@ -29,7 +48,10 @@ export default function Home(props: PageProps<never, State>) {
 				<Languages lang={language} />
 				<Timeline lang={language} />
 				<Photos lang={language} />
-				<Notes lang={language} />
+				<Notes
+					heading={notesHeading[language]}
+					notes={notes}
+				/>
 			</div>
 		</>
 	);
