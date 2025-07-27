867353f68f879d7ad61b95e66edfcdf48fbaf405
Felix <felix-schindler@outlook.com>
2025-07-27 21:48:52 +0200

    init svelte project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import LangSwitcher from '$lib/components/custom/lang-switcher.svelte';
+	import ModeSwitcher from '$lib/components/custom/mode-switcher.svelte';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.css';
+	import '../theme.css';
+
+	let { children, data } = $props();
+</script>
+
+<div>
+	<header class="flex flex-wrap items-center gap-2 p-4">
+		<ModeSwitcher />
+		<LangSwitcher currentLang={data.currentLang} />
+	</header>
+	<main class="px-4 md:p-0">
+		{@render children()}
+	</main>
+	<footer class="p-4"></footer>
+</div>
+
+<Toaster richColors position="bottom-center" />
+<ModeWatcher />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import Languages from '$lib/components/custom/languages.svelte';
+	import Technologies from '$lib/components/custom/technologies.svelte';
+	import BentoCard, { type BentoCardProps } from '$lib/components/magic/bento-card.svelte';
+	import BentoGrid from '$lib/components/magic/bento-grid.svelte';
+	import DockIcon from '$lib/components/magic/dock-icon.svelte';
+	import Dock from '$lib/components/magic/dock.svelte';
+	import Globe from '$lib/components/magic/globe.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import { t } from '$lib/i18n';
+	import {
+		CodeXml,
+		Github,
+		Gitlab,
+		Instagram,
+		Linkedin,
+		Mail,
+		MessageCircle
+	} from '@lucide/svelte';
+
+	const { data } = $props();
+
+	const features: BentoCardProps[] = [
+		{
+			Icon: MessageCircle,
+			Background: Languages,
+			name: 'home.languages.title',
+			description: 'home.languages.description',
+			class: 'col-span-3 lg:col-span-1'
+		},
+		{
+			Icon: CodeXml,
+			Background: Technologies,
+			name: 'home.technologies.title',
+			description: 'home.technologies.description',
+			class: 'col-span-3 lg:col-span-2'
+		}
+	];
+
+	let navs = [
+		{ label: 'GitHub', icon: Github, href: 'https://github.com/felix-schindler' },
+		{ label: 'GitLab', icon: Gitlab, href: 'https://gitlab.com/felix-schindler' },
+		{
+			label: 'LinkedIn',
+			icon: Linkedin,
+			href: 'https://www.linkedin.com/in/felix-schindler-vhwjpzf1z0fi73a/'
+		},
+		{ label: 'Instagram', icon: Instagram, href: 'https://www.instagram.com/vhwjpzf1z0fi73a' },
+		{ label: 'Mail', icon: Mail, href: 'mailto:webmaster@schindlerfelix.de' }
+	];
+</script>
+
+<div class="my-32">
+	<div class="flex w-full items-center justify-center">
+		<div
+			class="relative flex h-fit w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-40 pt-8 pb-40 md:pb-60 md:shadow-xl"
+		>
+			<span
+				class="pointer-events-none bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre-wrap text-transparent select-none dark:from-white dark:to-slate-900/10"
+			>
+				{$t('common.name')}
+			</span>
+			<Globe class="top-16" locations={data.locs} />
+			<div
+				class="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]"
+			></div>
+		</div>
+	</div>
+
+	<Dock class="my-8" let:mouseX let:distance let:magnification>
+		{#each navs as item (item.label)}
+			<DockIcon {mouseX} {magnification} {distance}>
+				<Tooltip.Provider delayDuration={0}>
+					<Tooltip.Root>
+						<Tooltip.Trigger>
+							{#snippet child({ props })}
+								<Button {...props} variant="ghost" class="size-11 rounded-full" href={item.href}>
+									<item.icon class="size-6 stroke-1" />
+								</Button>
+							{/snippet}
+						</Tooltip.Trigger>
+						<Tooltip.Content sideOffset={8}>
+							<p>{item.label}</p>
+						</Tooltip.Content>
+					</Tooltip.Root>
+				</Tooltip.Provider>
+			</DockIcon>
+		{/each}
+	</Dock>
+</div>
+
+<div class="mx-auto flex w-full items-center justify-center md:max-w-[90%] lg:max-w-[80%]">
+	<BentoGrid>
+		<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+		{#each features as item (item.name)}
+			<BentoCard {...item} />
+		{/each}
+	</BentoGrid>
+</div>
