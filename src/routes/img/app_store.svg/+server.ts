85c271ed8c72ad1536ebab1631dee2f136442d60
Felix <felix-schindler@outlook.com>
2025-07-29 16:10:19 +0200

    log path

--- src/routes/img/app_store.svg/+server.ts
+++ src/routes/img/app_store.svg/+server.ts
@@ -13,24 +13,24 @@ export const GET: RequestHandler = async ({ url }) => {
 	}
 
 	try {
-		return new Response(
-			await readFile(
-				join(
-					cwd(),
-					'src',
-					'routes',
-					'img',
-					'app_store.svg',
-					`${lang}_${dark ? 'white' : 'black'}.svg`
-				)
-			),
-			{
-				headers: {
-					'content-type': 'image/svg+xml'
-				}
-			}
+		const path = join(
+			cwd(),
+			'src',
+			'routes',
+			'img',
+			'app_store.svg',
+			`${lang}_${dark ? 'white' : 'black'}.svg`
 		);
-	} catch {
+
+		console.debug(path);
+
+		return new Response(await readFile(path), {
+			headers: {
+				'content-type': 'image/svg+xml'
+			}
+		});
+	} catch (e) {
+		console.error(e);
 		return Response.error();
 	}
 };
