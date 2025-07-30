3e61792bafdb34cdd7032d1ecbcb1bfbb9308feb
Felix <felix-schindler@outlook.com>
2025-07-30 23:11:40 +0200

    fix; imp; whatever

--- src/routes/projects/tanuki/support/+page.svelte
+++ src/routes/projects/tanuki/support/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import * as Card from '$lib/components/ui/card';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { Textarea } from '$lib/components/ui/textarea';
@@ -6,14 +7,24 @@
 	import { Send, Trash } from '@lucide/svelte';
 </script>
 
-<h2>{$t('tanuki.support')}</h2>
 <form
 	method="GET"
 	action="mailto:contact-project+felix-schindler-gitlab-ios-33025310-issue-@incoming.gitlab.com"
 	class="space-y-2"
 >
-	<Input placeholder={$t('tanuki.subject')} name="subject" />
-	<Textarea placeholder={$t('tanuki.message')} name="body" />
-	<Button type="submit"><Send /> {$t('tanuki.send')}</Button>
-	<Button type="reset" variant="secondary"><Trash /> {$t('tanuki.cancel')}</Button>
+	<Card.Root class="mx-auto max-w-3xl">
+		<Card.Header>
+			<Card.Title>
+				<h2 class="mb-0">{$t('tanuki.support')}</h2>
+			</Card.Title>
+		</Card.Header>
+		<Card.Content class="space-y-2">
+			<Input placeholder={$t('tanuki.subject')} name="subject" />
+			<Textarea placeholder={$t('tanuki.message')} name="body" />
+		</Card.Content>
+		<Card.Footer class="space-x-1">
+			<Button type="submit"><Send /> {$t('tanuki.send')}</Button>
+			<Button type="reset" variant="secondary"><Trash /> {$t('tanuki.cancel')}</Button>
+		</Card.Footer>
+	</Card.Root>
 </form>
