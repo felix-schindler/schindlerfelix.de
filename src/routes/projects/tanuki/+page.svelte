9ad1a5f045b77adcec4e75c61fc37f6c3b61e08c
Felix <felix-schindler@outlook.com>
2025-07-31 00:03:18 +0200

    Wrap Tanuki project page content in centered div

--- src/routes/projects/tanuki/+page.svelte
+++ src/routes/projects/tanuki/+page.svelte
@@ -23,54 +23,56 @@
 	] as const;
 </script>
 
-<h2>Links</h2>
-<div class="flex flex-wrap gap-2">
-	<Button href="https://gitlab.com/felix-schindler/gitlab-ios" variant="outline">
-		<Gitlab />
-		{$t('tanuki.gitlab_repo')}
-	</Button>
-	<Button href="/projects/tanuki/support" variant="outline">
-		<MessageCircleQuestion />
-		{$t('tanuki.support')}
-	</Button>
-	<Button href="/projects/tanuki/privacy" variant="outline">
-		<FileText />
-		{$t('tanuki.privacy')}
-	</Button>
+<div class="text-center sm:text-left">
+	<h2>Links</h2>
+	<div class="flex flex-wrap gap-2">
+		<Button href="https://gitlab.com/felix-schindler/gitlab-ios" variant="outline">
+			<Gitlab />
+			{$t('tanuki.gitlab_repo')}
+		</Button>
+		<Button href="/projects/tanuki/support" variant="outline">
+			<MessageCircleQuestion />
+			{$t('tanuki.support')}
+		</Button>
+		<Button href="/projects/tanuki/privacy" variant="outline">
+			<FileText />
+			{$t('tanuki.privacy')}
+		</Button>
+	</div>
+
+	<h2>Screenshots of v2 (unreleased)</h2>
+	<Carousel.Root>
+		<Carousel.Content class="-ml-1">
+			{#each imgsv2 as img (img)}
+				<Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
+					<div class="p-1">
+						<Card.Root
+							class="aspect-[9/19.5] bg-cover bg-center bg-no-repeat"
+							style="background-image: url(/img/tanuki/v2/{img})"
+						/>
+					</div>
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
+
+	<h2>Screenshots of v1</h2>
+	<Carousel.Root>
+		<Carousel.Content class="-ml-1">
+			{#each imgs as img (img)}
+				<Carousel.Item class="pl-1 sm:basis-1/2 md:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
+					<div class="p-1">
+						<Card.Root
+							class="aspect-[9/19.5] bg-cover bg-center bg-no-repeat"
+							style="background-image: url(/img/tanuki/{img})"
+						/>
+					</div>
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
 </div>
-
-<h2>Screenshots of v2 (unreleased)</h2>
-<Carousel.Root>
-	<Carousel.Content class="-ml-1">
-		{#each imgsv2 as img (img)}
-			<Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
-				<div class="p-1">
-					<Card.Root
-						class="aspect-[9/19.5] bg-cover bg-center bg-no-repeat"
-						style="background-image: url(/img/tanuki/v2/{img})"
-					/>
-				</div>
-			</Carousel.Item>
-		{/each}
-	</Carousel.Content>
-	<Carousel.Previous />
-	<Carousel.Next />
-</Carousel.Root>
-
-<h2>Screenshots of v1</h2>
-<Carousel.Root>
-	<Carousel.Content class="-ml-1">
-		{#each imgs as img (img)}
-			<Carousel.Item class="pl-1 sm:basis-1/2 md:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
-				<div class="p-1">
-					<Card.Root
-						class="aspect-[9/19.5] bg-cover bg-center bg-no-repeat"
-						style="background-image: url(/img/tanuki/{img})"
-					/>
-				</div>
-			</Carousel.Item>
-		{/each}
-	</Carousel.Content>
-	<Carousel.Previous />
-	<Carousel.Next />
-</Carousel.Root>
