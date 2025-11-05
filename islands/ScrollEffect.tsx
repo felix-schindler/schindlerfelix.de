ae6427675c5d57084926603e25231006c5936058
Felix <felix-schindler@outlook.com>
2025-11-05 11:46:45 +0100

    photos: bug fixes and landscape feature
    - fix parallax
    - ignore locations without pictures
    - improved grid
    - client-side: show landscape pictures in landscape

--- islands/ScrollEffect.tsx
+++ islands/ScrollEffect.tsx
@@ -7,6 +7,28 @@ export default function ScollEffect() {
 			// https://stackoverflow.com/a/3434388
 			globalThis.parallaxTitle.style.top = globalThis.scrollY * -0.9 + "px";
 		});
+
+		const grids = globalThis.document.getElementsByClassName("gallery-grid");
+		console.log("grids", grids.length);
+
+		for (const grid of grids) {
+			const imgs = grid.getElementsByTagName("img");
+			console.log("imgs", imgs.length);
+
+			for (const img of imgs) {
+				// decode() handles both loaded and not-yet-loaded images
+				img.decode().then(() => {
+					if (img.naturalWidth > img.naturalHeight) {
+						img.classList.add("landscape");
+					}
+				}).catch(() => {
+					// Fallback for browsers that don't support decode()
+					if (img.complete && img.naturalWidth > img.naturalHeight) {
+						img.classList.add("landscape");
+					}
+				});
+			}
+		}
 	}, []);
 
 	return null;
