2508fa5853ab30f5a1aac128db039c8fc4b70571
Felix <felix-schindler@outlook.com>
2026-07-08 16:36:04 +0200

    fix: page headings as h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,11 +38,11 @@
 		<div
 			class="relative flex h-fit w-full max-w-xl items-center justify-center overflow-hidden rounded-2xl border bg-background px-40 pt-8 pb-40 md:pb-60 md:shadow-xl"
 		>
-			<span
-				class="font-heading tracking-tighter pointer-events-none bg-linear-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre text-transparent select-none dark:from-white dark:to-slate-900/10"
+			<h1
+				class="tracking-tighter pointer-events-none bg-linear-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre text-transparent select-none dark:from-white dark:to-slate-900/10"
 			>
 				{$t('common.name')}
-			</span>
+			</h1>
 			<Globe class="top-16 sm:top-12" locations={data.locs} />
 			<div
 				class="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]"
