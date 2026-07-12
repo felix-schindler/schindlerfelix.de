f99acced76bb525a2475e14a46b6b42f9981b357
Felix <felix-schindler@outlook.com>
2026-07-12 22:35:40 +0200

    fix: unused imports

--- src/routes/projects/cup/+layout.svelte
+++ src/routes/projects/cup/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { t } from '$lib/i18n/index.js';
-	import { FileText, Github, House, MessageCircleHeart, MoveUpRight } from '@lucide/svelte';
+	import { FileText, Github, MessageCircleHeart, MoveUpRight } from '@lucide/svelte';
 	import { userPrefersMode } from 'mode-watcher';
 
 	const { data, children } = $props();
